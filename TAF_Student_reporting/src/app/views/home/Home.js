6266c9c395a37a0e57cc9133082befeb96020d17
yoonism49 <yoonism49@gmail.com>
2018-06-20 04:00:55 -0700

    Fixed 4 issues as of 06/20

--- TAF_Student_reporting/src/app/views/home/Home.js
+++ TAF_Student_reporting/src/app/views/home/Home.js
@@ -23,7 +23,7 @@ import HeatMap from 'react-heatmap-grid';
 import $ from 'jquery';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import Collapsible from 'react-collapsible';
-
+import axios from 'axios';
 
 export function getRange(count) {
   return Array.from({ length: count }, (_, i) => i);
@@ -126,10 +126,10 @@ class Home extends React.Component {
     fetchEarningGraphDataIfNeeded();
     fetchTeamMatesDataIfNeeded();
     const url = `http://ec2-54-193-65-106.us-west-1.compute.amazonaws.com:8080/student/fetchStudentAnswers?user_id=5b2001254e342d20e3dcb3c7`;
-    fetch(url)
-      .then(res => res.json())
-      .then(json => {
-        this.setState({rawData:json });
+    console.log('axios');
+    axios.get(url)
+      .then(res => {
+        this.setState({rawData:res.data });
       });
   }
 
@@ -140,8 +140,8 @@ class Home extends React.Component {
   componentDidUpdate() {
     
   }
-  reloadHeat() {
-      this.setState({heatData:null});
+  reloadHeat(index) {
+      this.setState({heatData:null,mod:index});
   }
   
   handleSelect(key) {
@@ -218,12 +218,15 @@ class Home extends React.Component {
     for (let i = 0; i < topicValue.length; i++) {
         if(topicValue[i]<30)
         {  
-          if(majorString.length===0) {
-            topicsTitle = 'Needs Major Remediation:';
-          }
-          topics += topicLabel[i];
-          if(i+1<topicValue.length) {
-            topics += ', ';
+          if(topicLabel[i] !== undefined) {
+            if(majorString.length===0) {
+              topicsTitle = 'Needs Major Remediation:';
+            }
+          
+            topics += topicLabel[i];
+            if(i<topicValue.length-2) {
+              topics += ', ';
+            }
           }
 
         }
@@ -255,13 +258,13 @@ class Home extends React.Component {
       <Tab>Most Recent</Tab>
       <Tab>Test History</Tab>
     </TabList>
-    <TabPanel title="Most Recent">
+    <TabPanel>
           <div
             className="row"
             style={{marginBottom: '5px'}}>
             <h2 className="testhistory-title">Test Results:</h2>
             <div className="col-md-3 topcard-left">
-              <div className="sm-st-info"><div>Test Name</div><span className="right-align-4">FC - Module 06</span></div>
+              <div className="sm-st-info"><div>Test Name</div><span className="testname">FC - Module 06</span></div>
             </div>
             <div className="col-md-2 topcard">
                <div className="sm-st-info"><div>Test Score</div><span className="right-align-3">{totalPass}/{correctness.length}</span></div>
@@ -278,40 +281,35 @@ class Home extends React.Component {
            
           </div>
 
-          <div className="row">
+          <div className="collapsible-row">
+               <Collapsible open trigger={<div className='collapsible-icon-second'><div className='bycollapse-title'><i className='fa fa-caret-right-collpase'></i>Topics</div> </div>}>
+       
             <div className="col-md-7 horizontalbar-div">
+       
               <ChartistGraph className={'ct-octave'} data={this.state.data} options={this.state.options} type={'Bar'} redraw={'true'} responsive={'true'}/>
+       
             </div>
             <div className="col-md-4 horizontalbar-div">
             <table class="table table-hover table-topic"><thead><tr><td>Class Avg</td><td># Questions</td><td># Points</td><td>Weight</td></tr></thead><tbody><tr><td>2</td><td>3</td><td>4</td><td>4</td></tr><tr><td>2</td><td>3</td><td>4</td><td>4</td></tr><tr><td>2</td><td>3</td><td>4</td><td>4</td></tr><tr><td>2</td><td>3</td><td>4</td><td>4</td></tr><tr><td>2</td><td>3</td><td>4</td><td>4</td></tr><tr><td>2</td><td>3</td><td>4</td><td>4</td></tr></tbody></table>
 
 
             </div>
+            </Collapsible>      
+      
           </div>
 
       <div className="row collapsible-row">
             <div className="col-md-12">
 
       
-       <Collapsible trigger={<div className='collapsible-icon-second'><div className='bycollapse-title'><i className='fa fa-caret-right-collpase'></i>Requirements Not Met</div> </div>}>
+       <Collapsible open trigger={<div className='collapsible-icon-second'><div className='bycollapse-title'><i className='fa fa-caret-right-collpase'></i>Requirements Not Met</div> </div>}>
        <div className="collapsible-paragraph">
        {rows}
        </div>
       </Collapsible>      
             </div>
       </div>
- {/*  
-          <div className="row">
-            <div className="col-md-5">
-              <TeamMatesDemo
-                isFetching={teamMatesIsFetching}
-                members={teamMates}
-              />
-            </div>
-            <div className="col-md-7">
-              <TodoListDemo />
-            </div>
-          </div>*/}
+
          </TabPanel>
          <TabPanel title="Test History">
          <div className="horiz-scroll-module">
@@ -320,65 +318,53 @@ class Home extends React.Component {
             className="row horiz-model-inside"
             style={{marginBottom: '5px'}}>
             <h2 className="testhistory-title">Test History:</h2>
-           <div className="col-md-1 topmodule-left">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 01</span></div>
+           <div className={this.state.mod==='1'?'col-md-1 topmodule-left':this.state.mod===undefined?'col-md-1 topmodule-left':'col-md-1 topmodule-first'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this, '1')}><span>Module 01</span></div>
             </div>
-            <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 02</span></div>
+            <div className={this.state.mod==='2'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'2')}><span>Module 02</span></div>
             </div>
-             <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 03</span></div>
+             <div className={this.state.mod==='3'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'3')}><span>Module 03</span></div>
             </div>
-            <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 04</span></div>
+            <div className={this.state.mod==='4'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'4')}><span>Module 04</span></div>
             </div>
-             <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 05</span></div>
+             <div className={this.state.mod==='5'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'5')}><span>Module 05</span></div>
             </div>
-            <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 06</span></div>
+            <div className={this.state.mod==='6'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'6')}><span>Module 06</span></div>
             </div>
-             <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 07</span></div>
+             <div className={this.state.mod==='7'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'7')}><span>Module 07</span></div>
             </div>
-            <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 08</span></div>
+            <div className={this.state.mod==='8'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'8')}><span>Module 08</span></div>
             </div>
-             <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 09</span></div>
+             <div className={this.state.mod==='9'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'9')}><span>Module 09</span></div>
             </div>
-            <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 10</span></div>
+            <div className={this.state.mod==='10'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'10')}><span>Module 10</span></div>
             </div>
-             <div className="col-md-1 topmodule">
-               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this)}><span>Module 11</span></div>
+             <div className={this.state.mod==='11'?'col-md-1 topmodule-left-orange':'col-md-1 topmodule'}>
+               <div className="sm-st-info" onClick = {this.reloadHeat.bind(this,'11')}><span>Module 11</span></div>
             </div>
             </div>
            
           </div>
 
-          <div className="row">
-            <div className="col-md-12">
-            <div className="horiz-scroll">
-             <div className="heatmap-div">
-             <HeatMap
-              xLabels={xLabels}
-              yLabels={yLabels}
-              data={data}
-              background={'green'}
-            />
-            </div>
-            </div>
-            </div>
-          </div>
-
-          <div className="row">
+           <div className="collapsible-row-log ">
+               <Collapsible open trigger={<div className='collapsible-icon-second'><div className='bycollapse-title'><i className='fa fa-caret-right-collpase'></i>Topics</div> </div>}>
+       
             <div className="col-md-12">
               <div className="panel-body table-responsive test-log-student">
               <h3 className="panel-heading">Test Log</h3>
 <table className="table table-hover"><thead><tr><td>Class</td><td>Test Name</td><td>Date Completed</td><td>Attempts</td><td>Total Score</td><td>Result</td><td></td></tr></thead><tbody><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr><tr><td className="text-align-left">Class 3</td><td className="text-align-left">FC - Module 02</td><td className="text-align-left" className="text-align-left">11/20/2014</td><td className="text-align-right">4</td><td className="text-align-right">0</td><td className="text-align-left">Fail</td><td className="text-align-left"><div id="sm-st-info-button">More</div></td></tr></tbody></table></div>
             
             </div>
+            </Collapsible>
           </div>
        </TabPanel>
         </Tabs>
